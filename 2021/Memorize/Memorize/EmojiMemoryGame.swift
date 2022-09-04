08dd90666e3551efe7b1b5958e8b447c5a77f6da
Preshant Achuthan <engerium@gmail.com>
2022-09-04 17:10:07 +0800

    Midway progress of Lecture 4

--- 2021/Memorize/Memorize/EmojiMemoryGame.swift
+++ 2021/Memorize/Memorize/EmojiMemoryGame.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 
-class EmojiMemoryGame {
+class EmojiMemoryGame: ObservableObject {
     static let emojis = ["🚗", "🚎", "🏎", "🚑", "🛴", "🛵", "🚔", "🚍", "🚖", "🚠", "🚝", "🚂", "🚁", "🛩", "🚀",  "⛵️", "🛳"]
     
     static func createMemoryGame() -> MemoryGame<String> {
@@ -17,9 +17,16 @@ class EmojiMemoryGame {
         }
     }
     
-    private(set) var model: MemoryGame<String> = createMemoryGame()
+    @Published private var model: MemoryGame<String> = createMemoryGame()
     
     var cards: Array<MemoryGame<String>.Card> {
-        return model.cards
+        model.cards
     }
+    
+    // MARK: - Intent(s)
+    
+    func choose(_ card: MemoryGame<String>.Card) {
+        model.choose(card)
+    }
+    
 }
--- 2021/Memorize/Memorize/MemoryGame.swift
+++ 2021/Memorize/Memorize/MemoryGame.swift
@@ -10,8 +10,20 @@ import Foundation
 struct MemoryGame<CardContent> {
     private(set) var cards: Array<Card>
     
-    func choose(_ card: Card) {
-        
+    mutating func choose(_ card: Card) {
+        let chosenIndex = index(of: card)
+        cards[chosenIndex].isFaceUp.toggle()
+        print("\(cards)")
+    }
+    
+    func index(of card: Card) -> Int {
+        for index in 0..<cards.count {
+            if cards[index].id == card.id {
+                return index
+                
+            }
+        }
+        return 0 // BOGUS
     }
     
     init(numberOfPairsOfCards: Int, createCardContent: (Int) -> CardContent) {
@@ -20,14 +32,15 @@ struct MemoryGame<CardContent> {
         // add numberOfPairsOfCards x2 to cards array
         for pairIndex in 0..<numberOfPairsOfCards {
             let content: CardContent = createCardContent(pairIndex)
-            cards.append(Card(content: <#T##CardContent#>))
-            cards.append(Card(content: <#T##CardContent#>))
+            cards.append(Card(content: content, id: pairIndex * 2))
+            cards.append(Card(content: content, id: pairIndex * 2 + 1))
         }
     }
     
-    struct Card {
-        var isFaceUp: Bool = false
+    struct Card: Identifiable {
+        var isFaceUp: Bool = true
         var isMatched: Bool = false
         var content: CardContent
+        var id: Int
     }
 }
