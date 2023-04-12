fbb295ac50741ad1f2bc61e3b727bde2d4118d52
Preshant Achuthan <engerium@gmail.com>
2023-04-12 14:02:36 +0800

    L5 - Re-pushed

--- 2023/Memorize/Memorize/EmojiMemoryGameView.swift
+++ 2023/Memorize/Memorize/EmojiMemoryGameView.swift
@@ -24,44 +24,44 @@ struct EmojiMemoryGameView: View {
         }
         .foregroundColor(/*@START_MENU_TOKEN@*/.red/*@END_MENU_TOKEN@*/)
         .padding(.horizontal)
+        
     }
-}
-
-struct CardView: View {
-    let card: EmojiMemoryGame.Card
     
-    var body: some View {
-        GeometryReader { geomerty in
-            ZStack {
-                let shape = RoundedRectangle(cornerRadius: DrawingConstants.cornerRadius)
-                if card.isFaceUp {
-                    shape.fill(.white)
-                    shape.strokeBorder(lineWidth: DrawingConstants.lineWidth)
-                    Text(card.content).font(font(in: geomerty.size))
-                } else if card.isMatched {
-                    shape.opacity(0)
-                } else {
-                    shape.fill()
+    struct CardView: View {
+        let card: EmojiMemoryGame.Card
+        
+        var body: some View {
+            GeometryReader { geomerty in
+                ZStack {
+                    let shape = RoundedRectangle(cornerRadius: DrawingConstants.cornerRadius)
+                    if card.isFaceUp {
+                        shape.fill(.white)
+                        shape.strokeBorder(lineWidth: DrawingConstants.lineWidth)
+                        Text(card.content).font(font(in: geomerty.size))
+                    } else if card.isMatched {
+                        shape.opacity(0)
+                    } else {
+                        shape.fill()
+                    }
                 }
             }
         }
+        
+        private func font(in size: CGSize) -> Font {
+            Font.system(size: min(size.width, size.height) * DrawingConstants.fontScale)
+        }
+        
+        private struct DrawingConstants {
+            static let cornerRadius: CGFloat = 20
+            static let lineWidth: CGFloat = 3
+            static let fontScale: CGFloat = 0.8
+        }
     }
     
-    private func font(in size: CGSize) -> Font {
-        Font.system(size: min(size.width, size.height) * DrawingConstants.fontScale)
-    }
-    
-    private struct DrawingConstants {
-        static let cornerRadius: CGFloat = 20
-        static let lineWidth: CGFloat = 3
-        static let fontScale: CGFloat = 0.8
+    struct EmojiMemoryGameView_Previews: PreviewProvider {
+        static var previews: some View {
+            let game = EmojiMemoryGame()
+            EmojiMemoryGameView(game: game)
+        }
     }
 }
-
-struct EmojiMemoryGameView_Previews: PreviewProvider {
-    static var previews: some View {
-        let game = EmojiMemoryGame()
-        EmojiMemoryGameView(game: game)
-    }
-}
-
