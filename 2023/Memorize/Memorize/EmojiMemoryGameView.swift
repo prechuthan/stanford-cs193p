87b2287c7bdf1ff990decc48474ac6f8e32706b9
Preshant Achuthan <engerium@gmail.com>
2023-06-20 18:45:05 +0800

    Completed L7 - Added animation to card text

--- 2023/Memorize/Memorize/EmojiMemoryGameView.swift
+++ 2023/Memorize/Memorize/EmojiMemoryGameView.swift
@@ -34,18 +34,24 @@ struct EmojiMemoryGameView: View {
                 ZStack {
                     Pie(startAngle: Angle(degrees: 0 - 90), endAngle: Angle(degrees: 110 - 90))
                         .padding(DrawingConstants.timerCirclePadding).opacity(DrawingConstants.timerCircleOpacity)
-                    Text(card.content).font(font(in: geomerty.size))
+                    Text(card.content)
+                        .rotationEffect(Angle.degrees(card.isMatched ? 360: 0))
+                        .animation(Animation.linear(duration: 1).repeatForever(autoreverses: false))
+                        .font(Font.system(size: DrawingConstants.fontSize))
+                        .scaleEffect(scale(thatFits: geomerty.size))
                 }
                 .cardify(isFaceUp: card.isFaceUp)
             }
         }
         
-        private func font(in size: CGSize) -> Font {
-            Font.system(size: min(size.width, size.height) * DrawingConstants.fontScale)
+        private func scale(thatFits size: CGSize) -> CGFloat {
+            min(size.width, size.height) / (DrawingConstants.fontSize / DrawingConstants.fontScale)
         }
+        
         
         private struct DrawingConstants {
             static let fontScale: CGFloat = 0.6
+            static let fontSize: CGFloat = 32
             static let timerCirclePadding: CGFloat = 5
             static let timerCircleOpacity: CGFloat = 0.5
         }
