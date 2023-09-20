79b39412ffa51d80d23791d75f779e91065a41e4
Preshant Achuthan <engerium@gmail.com>
2023-09-20 20:55:16 +0800

    Midway L1 - Rounded Rect [1:10:33]

---
+++ 2023/Memorize/Memorize/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Preshant Achuthan on 20/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(lineWidth: 3)
+                .padding(.horizontal)
+                .foregroundColor(.red)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
